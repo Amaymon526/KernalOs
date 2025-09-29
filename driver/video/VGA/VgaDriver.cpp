054037221f5875872e1729fe1d598aaf5ddb89ec
jeff <jeffrey.nelwek@varias.at>
2025-09-29 14:46:25 +0200

    VgaTreiber erweitert

--- driver/video/VGA/VgaDriver.cpp
+++ driver/video/VGA/VgaDriver.cpp
@@ -17,7 +17,36 @@ void VgaDriver::putChar(int row, int col, char c, char color) {
 void VgaDriver::printString(int startRow, int startCol, const char* text, char color) {
     int i = 0;
     while (text[i] != '\0') {
-        writeChar(startRow, startCol + i, text[i], color);  // << + i !
+        writeChar(startRow, startCol + i, text[i], color);
+        i++;
+    }
+}
+
+void VgaDriver::clearScreen() {
+    volatile char* vga = (volatile char*)0xB8000;
+
+    for (int row = 0; row < 25; row++) {
+        for (int col = 0; col < 80; col++) {
+            int index = (row * 80 + col) * 2;
+            vga[index] = ' ';
+
+        }
+    }
+
+}
+    void VgaDriver::printCentered(const char* text, int row, char color) {
+    int length = 0;
+    while (text[length] != '\0') {
+        length++;
+    }
+
+    // Startspalte berechnen
+    int startCol = (80 - length) / 2;
+
+    // Text ausgeben
+    int i = 0;
+    while (text[i] != '\0') {
+        writeChar(row, startCol + i, text[i], color);
         i++;
     }
 }
--- driver/video/VGA/VgaDriver.hpp
+++ driver/video/VGA/VgaDriver.hpp
@@ -10,6 +10,8 @@ namespace vga_driver {
     public:
         void putChar(int row, int col, char c, char color);
         void printString(int startRow, int startCol, const char* text, char color);
+        void printCentered(const char* text, int row, char color);
+        void clearScreen();
 
     private:
         void writeChar(int row, int col, char c, char color);
